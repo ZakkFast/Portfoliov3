e396ed9456c6472bb226a160b9792b5b61ed5966
Zakk Fast <zakkfastpro@gmail.com>
2022-04-21 13:17:49 -0700

    created home component and layout

---
+++ src/components/Home.jsx
@@ -0,0 +1,37 @@
+
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-pink-600'>Hey there, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+          Zakk Fast
+        </h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+          I'm a Full Stack Developer.
+        </h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+          I am a solution-driven Full-Stack Developer. Specializing in creating
+          striking and extra-ordinary digital experiences. At the moment I am
+          building scalable, full-stack applications using the MERN stack.
+          However, my curious nature is always leading to new tech being added
+          to my Web Dev Aresenal.
+        </p>
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+            View Work
+            <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight className='ml-3 ' />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
