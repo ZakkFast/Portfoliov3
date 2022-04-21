9b93e6e673dc13cc20e57bf31b84464f45c172b9
Zakk Fast <zakkfastpro@gmail.com>
2022-04-21 15:13:05 -0700

    created contact form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import About from "./components/About";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import Skills from "./components/Skills";
-import Work from "./components/Work";
-
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Skills from './components/Skills';
+import Work from './components/Work';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div>
-    <Navbar />
-    <Home />
-    <About />
-    <Skills />
-    <Work / >
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
     </div>
   );
 }
