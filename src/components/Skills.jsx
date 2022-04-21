fdbe4a2e968521ed8f6e9d60a1ad12703fccdf73
Zakk Fast <zakkfastpro@gmail.com>
2022-04-21 15:26:11 -0700

    removed typo causing failed deployment

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -16,7 +16,7 @@ const Skills = () => {
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
             My Skill Set
           </p>
-          <p className="py-4">// Here's the tech I work with most.</p>
+          <p className="py-4">Here's the tech I work with most.</p>
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
