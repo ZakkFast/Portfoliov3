2e7b419f776a64eae828515cee4189281f3b5e8f
Zakk Fast <zakkfastpro@gmail.com>
2022-04-21 15:49:35 -0700

    changed colors and animations

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,20 @@
-
 import React from 'react';
-import { HiArrowNarrowRight } from 'react-icons/hi';
+import { HiArrowNarrowDown } from 'react-icons/hi';
+import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+    <div name="home" className="w-full h-screen bg-[#9bc1bc]">
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-pink-600'>Hey there, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-[#333]">Hey there, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ed6a5a]">
           Zakk Fast
         </h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#333]">
           I'm a Full Stack Developer.
         </h2>
-        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+        <p className="text-[#333] py-4 max-w-[700px]">
           I am a solution-driven Full-Stack Developer. Specializing in creating
           striking and extra-ordinary digital experiences. At the moment I am
           building scalable, full-stack applications using the MERN stack.
@@ -22,12 +22,14 @@ const Home = () => {
           to my Web Dev Aresenal.
         </p>
         <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-            View Work
-            <span className='group-hover:rotate-90 duration-300'>
-              <HiArrowNarrowRight className='ml-3 ' />
-            </span>
-          </button>
+          <Link to="work" smooth={true} duration={500}>
+            <button className="text-[#ed6a5a] group border-2 border-[#333] px-6 py-3 my-2 flex items-center font-bold hover:bg-[#ed6a5a] hover:text-[#333] hover:border-[#ed6a5a]">
+              View Work
+              <span className="animate-pulse">
+                <HiArrowNarrowDown className="ml-3 " />
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
