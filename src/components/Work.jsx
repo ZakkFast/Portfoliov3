40317c6b3eaee249933de9b1ad33ea84dd14b302
Zakk Fast <zakkfastpro@gmail.com>
2022-04-25 15:36:03 -0700

    fixed react specfici error with norel

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -11,7 +11,9 @@ const Work = () => {
           <p className="text-4xl font-bold inline border-b-4 border-[#ed6a5a]">
             Projects
           </p>
-          <p className="py-6">Here is some of my recent work. Hover over to see more!</p>
+          <p className="py-6">
+            Here is some of my recent work. Hover over to see more!
+          </p>
         </div>
 
         {/* Grid Container */}
@@ -27,12 +29,20 @@ const Work = () => {
                 Devvy
               </span>
               <div className="pt-8 text-center">
-                <a href="https://afternoon-citadel-56015.herokuapp.com/" target='_blank'>
+                <a
+                  href="https://afternoon-citadel-56015.herokuapp.com/"
+                  target="_blank"
+                  rel="noopener"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="https://github.com/ZakkFast/UW-21-22-Dev-Connect" target='_blank'>
+                <a
+                  href="https://github.com/ZakkFast/UW-21-22-Dev-Connect"
+                  target="_blank"
+                  rel="noopener"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
@@ -42,21 +52,29 @@ const Work = () => {
           </div>
           <div
             style={{ backgroundImage: `url(${Project2})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
                 Defi
               </span>
-              <div className='pt-8 text-center'>
-                <a href='https://6267202445bcea03be5f36f2--playful-pithivier-ec077b.netlify.app/' target='_blank'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://6267202445bcea03be5f36f2--playful-pithivier-ec077b.netlify.app/"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href='https://github.com/ZakkFast/defi' target='_blank'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                <a
+                  href="https://github.com/ZakkFast/defi"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
