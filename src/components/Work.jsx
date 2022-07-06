41ec28de2355cd21c448ec93468780b3aad3af60
Zakk Fast <Zakkfastpro@gmail.com>
2022-07-05 23:50:58 -0700

    Updating Projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Project1 from '../assets/Project1.png';
 import Project2 from '../assets/Project2.png';
+import Project3 from '../assets/Project3.png';
 
 const Work = () => {
   return (
@@ -81,6 +82,37 @@ const Work = () => {
               </div>
             </div>
           </div>
+          <div
+            style={{ backgroundImage: `url(${Project3})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Expense Tracker
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://62c52daefe33732e5980af94--creative-axolotl-4a6607.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/ZakkFast/react-expense-tracker"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -88,3 +120,4 @@ const Work = () => {
 };
 
 export default Work;
+
